895c94b3b48ab631f8f2e4eea451a7c057e5a659
healygi <healygi@tcd.ie>
2022-05-24 11:25:35 +0000

    button test

---
+++ scripts/tests/button.test.js
@@ -0,0 +1,14 @@
+const buttonClick = require("../tests/button");
+
+beforeEach( () => {
+    document.body.innerHTML = "<p id='par'></p>";
+
+});
+
+describe("DOM test", () => {
+    test("expects p content to change", () => {
+        buttonClick();
+        expect(document.getElementById("par").innerHTML).toEqual("You Clicked")
+    })
+
+});
